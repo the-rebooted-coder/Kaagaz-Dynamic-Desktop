2275cf5421fb5e69e4135dcd5481dabcc1301936
Spandan Saxena <spandansaxena@gmail.com>
2023-08-20 19:52:25 +0530

    Wallpaper being set by press of a button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,7 @@
 ﻿using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using System;
 
 namespace Dynamic_Desktop
 {
@@ -6,13 +9,11 @@ namespace Dynamic_Desktop
     {
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
-        
+        private string morningWallpaper = "";
+        private string afternoonWallpaper = "";
+        private string eveningWallpaper = "";
+        private string nightWallpaper = "";
         private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -28,14 +29,8 @@ namespace Dynamic_Desktop
                 // Get the current hour of the day
                 int currentHour = DateTime.Now.Hour;
 
-                // Define paths to your wallpaper images for different times of the day
-                string morningWallpaper = @"C:\Users\NGC\Downloads\wall_after.png";
-                string afternoonWallpaper = @"C:\Users\NGC\Downloads\wall_after.png";
-                string eveningWallpaper = @"C:\Users\NGC\Downloads\wall_after.png";
-                string nightWallpaper = @"C:\Path\To\NightWallpaper.jpg";
-
-                // Set the appropriate wallpaper based on the current time
-                string wallpaperPath = GetWallpaperPath(currentHour, morningWallpaper, afternoonWallpaper, eveningWallpaper, nightWallpaper);
+                // Determine which wallpaper to use based on the current time
+                string wallpaperPath = GetWallpaperPath(currentHour);
 
                 // Call the SystemParametersInfo function to set the wallpaper
                 SystemParametersInfo(0x0014, 0, wallpaperPath, 0x0001 | 0x0002);
@@ -44,23 +39,23 @@ namespace Dynamic_Desktop
                 Thread.Sleep(3600000); // 1 hour in milliseconds
             }
         }
-        private string GetWallpaperPath(int hour, string morning, string afternoon, string evening, string night)
+        private string GetWallpaperPath(int hour)
         {
             if (hour >= 5 && hour < 12)
             {
-                return morning; // Morning (5 AM to 11:59 AM)
+                return morningWallpaper; // Morning (5 AM to 11:59 AM)
             }
             else if (hour >= 12 && hour < 17)
             {
-                return afternoon; // Afternoon (12 PM to 4:59 PM)
+                return afternoonWallpaper; // Afternoon (12 PM to 4:59 PM)
             }
             else if (hour >= 17 && hour < 20)
             {
-                return evening; // Evening (5 PM to 7:59 PM)
+                return eveningWallpaper; // Evening (5 PM to 7:59 PM)
             }
             else
             {
-                return night; // Night (8 PM to 4:59 AM)
+                return nightWallpaper; // Night (8 PM to 4:59 AM)
             }
         }
         private void StartWallpaperChanger()
@@ -68,20 +63,104 @@ namespace Dynamic_Desktop
             Thread wallpaperThread = new Thread(ChangeWallpaper);
             wallpaperThread.Start();
         }
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            chooseMorningWallpaperButton = new Button();
+            chooseAfternoonWallpaperButton = new Button();
+            chooseEveningWallpaperButton = new Button();
+            chooseNightWallpaperButton = new Button();
+            label1 = new Label();
+            setWallpaperButton = new Button();
+            SuspendLayout();
+            // 
+            // chooseMorningWallpaperButton
+            // 
+            chooseMorningWallpaperButton.Location = new Point(274, 61);
+            chooseMorningWallpaperButton.Name = "chooseMorningWallpaperButton";
+            chooseMorningWallpaperButton.Size = new Size(138, 33);
+            chooseMorningWallpaperButton.TabIndex = 1;
+            chooseMorningWallpaperButton.Text = "Morning Image";
+            chooseMorningWallpaperButton.UseVisualStyleBackColor = true;
+            chooseMorningWallpaperButton.Click += chooseMorningWallpaperButton_Click_1;
+            // 
+            // chooseAfternoonWallpaperButton
+            // 
+            chooseAfternoonWallpaperButton.Location = new Point(274, 100);
+            chooseAfternoonWallpaperButton.Name = "chooseAfternoonWallpaperButton";
+            chooseAfternoonWallpaperButton.Size = new Size(138, 33);
+            chooseAfternoonWallpaperButton.TabIndex = 2;
+            chooseAfternoonWallpaperButton.Text = "Afternoon Image";
+            chooseAfternoonWallpaperButton.UseVisualStyleBackColor = true;
+            chooseAfternoonWallpaperButton.Click += chooseAfternoonWallpaperButton_Click_1;
+            // 
+            // chooseEveningWallpaperButton
+            // 
+            chooseEveningWallpaperButton.Location = new Point(274, 139);
+            chooseEveningWallpaperButton.Name = "chooseEveningWallpaperButton";
+            chooseEveningWallpaperButton.Size = new Size(138, 33);
+            chooseEveningWallpaperButton.TabIndex = 3;
+            chooseEveningWallpaperButton.Text = "Evening Image";
+            chooseEveningWallpaperButton.UseVisualStyleBackColor = true;
+            chooseEveningWallpaperButton.Click += chooseEveningWallpaperButton_Click_1;
+            // 
+            // chooseNightWallpaperButton
+            // 
+            chooseNightWallpaperButton.Location = new Point(274, 178);
+            chooseNightWallpaperButton.Name = "chooseNightWallpaperButton";
+            chooseNightWallpaperButton.Size = new Size(138, 33);
+            chooseNightWallpaperButton.TabIndex = 4;
+            chooseNightWallpaperButton.Text = "Night Image";
+            chooseNightWallpaperButton.UseVisualStyleBackColor = true;
+            chooseNightWallpaperButton.Click += chooseNightWallpaperButton_Click_1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(196, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(304, 32);
+            label1.TabIndex = 5;
+            label1.Text = "Kaagaz Dynamic Desktop";
+            // 
+            // setWallpaperButton
+            // 
+            setWallpaperButton.BackColor = SystemColors.InactiveCaptionText;
+            setWallpaperButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            setWallpaperButton.ForeColor = SystemColors.ButtonHighlight;
+            setWallpaperButton.Location = new Point(257, 231);
+            setWallpaperButton.Name = "setWallpaperButton";
+            setWallpaperButton.Size = new Size(169, 33);
+            setWallpaperButton.TabIndex = 6;
+            setWallpaperButton.Text = "Kaagaz!";
+            setWallpaperButton.TextImageRelation = TextImageRelation.TextBeforeImage;
+            setWallpaperButton.UseVisualStyleBackColor = false;
+            setWallpaperButton.Click += setWallpaperButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.MenuHighlight;
+            ClientSize = new Size(711, 356);
+            Controls.Add(setWallpaperButton);
+            Controls.Add(label1);
+            Controls.Add(chooseNightWallpaperButton);
+            Controls.Add(chooseEveningWallpaperButton);
+            Controls.Add(chooseAfternoonWallpaperButton);
+            Controls.Add(chooseMorningWallpaperButton);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
-        #endregion
+        private Button chooseMorningWallpaperButton;
+        private Button chooseAfternoonWallpaperButton;
+        private Button chooseEveningWallpaperButton;
+        private Button chooseNightWallpaperButton;
+        private Label label1;
+        private Button setWallpaperButton;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -7,5 +7,84 @@ namespace Dynamic_Desktop
             InitializeComponent();
             StartWallpaperChanger();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chooseEveningWallpaperButton_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp|All Files|*.*",
+                Title = "Select Evening Wallpaper Image",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                eveningWallpaper = openFileDialog.FileName;
+            }
+        }
+
+        private void chooseMorningWallpaperButton_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp|All Files|*.*",
+                Title = "Select Morning Wallpaper Image",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                morningWallpaper = openFileDialog.FileName;
+            }
+        }
+
+        private void chooseAfternoonWallpaperButton_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp|All Files|*.*",
+                Title = "Select Afternoon Wallpaper Image",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                afternoonWallpaper = openFileDialog.FileName;
+            }
+        }
+
+        private void chooseNightWallpaperButton_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp|All Files|*.*",
+                Title = "Select Night Wallpaper Image",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                nightWallpaper = openFileDialog.FileName;
+            }
+        }
+
+        private void setWallpaperButton_Click(object sender, EventArgs e)
+        {
+            // Get the current hour of the day
+            int currentHour = DateTime.Now.Hour;
+
+            // Determine which wallpaper to use based on the current time
+            string wallpaperPath = GetWallpaperPath(currentHour);
+
+            // Call the SystemParametersInfo function to set the wallpaper
+            SystemParametersInfo(0x0014, 0, wallpaperPath, 0x0001 | 0x0002);
+
+            MessageBox.Show("Wallpaper set successfully!");
+        }
     }
 }
